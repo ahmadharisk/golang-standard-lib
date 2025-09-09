0aec3cc408bcfa5fcac4c515b115f70931216bf6
Peppo <ahmadharisk@gmail.com>
2025-09-09 09:49:30 +0700

    hari 7, menggunakan package strings

---
+++ strings.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains("test", "es"))
+	fmt.Println(strings.Split("peppo silpiana", " "))
+	fmt.Println(strings.Trim("    peppo    ", " "))
+}
