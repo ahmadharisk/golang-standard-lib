661535abd050e03f3fe0767821155b47a1c7676a
Peppo <ahmadharisk@gmail.com>
2025-09-09 20:43:47 +0700

    hari 7, menggunakan package container list

---
+++ list.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	data := list.New()
+	data.PushBack(1)
+	data.PushBack(2)
+	data.PushBack(3)
+
+	for e := data.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+
+	// manual
+	head := data.Front()
+	fmt.Println(head.Value)
+}
